3181c94e948633e4a6597af6241e624536d1a80e
Catarina Esposito Mendes <catarina@Catarinas-MacBook-Air.local>
2022-04-13 17:48:30 -0400

    int

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int myValue = 10;
+
+        int myMinValue = Integer.MIN_VALUE;
+        int myMaxValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer Minimum Value is " + myMinValue );
+        System.out.println("Integer Maximum Value is " + myMaxValue );
     }
 }
