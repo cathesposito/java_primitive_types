6ba2b5a9e171d244b24d0ef77dfb66d96f3a35f6
Catarina Esposito Mendes <catarina@Catarinas-MacBook-Air.local>
2022-04-13 18:01:49 -0400

    byte

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
         int myValue = 10;
 
-        int myMinValue = Integer.MIN_VALUE;
-        int myMaxValue = Integer.MAX_VALUE;
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
 
-        System.out.println("Integer Minimum Value is " + myMinValue );
-        System.out.println("Integer Maximum Value is " + myMaxValue );
+        System.out.println("Integer Minimum Value is " + myMinByteValue );
+        System.out.println("Integer Maximum Value is " + myMaxByteValue );
     }
 }
