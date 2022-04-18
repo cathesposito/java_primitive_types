fbe980a1abf147b9907ea74af2f436e296dbe4fe
Catarina Esposito Mendes <catarina@Catarinas-MacBook-Air.local>
2022-04-18 19:12:10 -0400

    char variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        float myMinFloatValue = Float.MIN_VALUE;
-        float myMaxFloatValue = Float.MAX_VALUE;
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
 
-        double myMinDoubleValue = Double.MIN_VALUE;
-        double myMaxDoubleValue = Double.MAX_VALUE;
-
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
-
+        System.out.println(myChar + " is equal to " + myUnicodeChar);
     }
 }
